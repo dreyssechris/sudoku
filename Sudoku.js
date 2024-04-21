cf19c270f168c9fa1377211c6da0e484aa6ba860
Chriz <chrisdreysse@gmail.com>
2024-04-21 15:17:57 +0200

    new error message, if the input sudoku is invalid

--- Sudoku.js
+++ Sudoku.js
@@ -12,7 +12,8 @@ class Sudoku {
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0]
         ];
-        // save to grid from user input, so that all upcomming numbers can receive a different coloring
+        // save to grid from user input, 
+        // so that the grid can be reset to this (also for individual coroling of the solution)
         this.originalGrid = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,69 +25,14 @@ class Sudoku {
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0]
         ];
-        // x = 0 - 2
-        // y = 0 - 2
-        this.grid1 = [
+        // 3x3 grid to check if certain value is unique inside of it
+        this.smallerGrid = [
             [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]
         ];
-        // x = 0 - 2 
-        // y = 3 - 5
-        this.grid2 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ];
-        // x = 0 - 2 
-        // y = 6 - 8
-        this.grid3 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ];
-        // x = 3 - 5 
-        // y = 0 - 2
-        this.grid4 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]; 
-        // x = 3 - 5 
-        // y = 3 - 5
-        this.grid5 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]; 
-        // x = 3 - 5 
-        // y = 6 - 8
-        this.grid6 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ];
-        // x = 6 - 8 
-        // y = 0 - 2 
-        this.grid7 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]; 
-        // x = 6 - 8 
-        // y = 3 - 5
-        this.grid8 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]; 
-        // x = 6 - 8 
-        // y = 6 - 8
-        this.grid9 = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]; 
+        this.errorMessage = 
+        "Invalid Sudoku: Values are not unique in row, column, or 3x3 grid.";
     }
 
     // Fields with invalid input (!1-9) are countet as empty (0)
@@ -124,7 +70,12 @@ class Sudoku {
     }
 
     // Method to call isValid() and check if a certain number is valid for the cell
+    // Also check if the Sudoku can be solved in the first place
     solve() {
+        if(!this.isSolveable()) {
+            this.showToast(this.errorMessage);
+            return false;
+        }
         for (let x = 0; x < 9; x++) {
             for (let y = 0; y < 9; y++) {
                 if (this.grid[x][y] === 0) {
@@ -134,15 +85,16 @@ class Sudoku {
                             if (this.solve()) {
                                 return true; 
                             } else {
-                                this.grid[x][y] = 0; // reset to try different number
+                                // reset to try different number
+                                this.grid[x][y] = 0;
                             }
                         }
                     }
-                    return false; // Keine gültige Zahl gefunden, unerreichbarer Zustand
+                    return false; 
                 }
             }
         }
-        return true; // Alle Felder gefüllt, Rätsel gelöst
+        return true; // when all field are filled 
     }
 
     // Called when `Whipe` was pressed - whipes all input fields
@@ -166,8 +118,20 @@ class Sudoku {
             }
             document.getElementById(`cell-${i}`).style.color = "black"; 
         }
-        
         console.log(this.originalGrid); 
+    }
+
+    showToast(message) {
+        const error = document.getElementById("error");
+        error.innerHTML = message;
+        error.style.visibility = "visible";
+    
+        // Message should disappear after `x` seconds
+        setTimeout(function() {
+            error.style.visibility = "hidden";
+            // So that the Sudoku has the same gap to the h1 as before
+            error.innerHTML = ""; 
+        }, 5000);
     }
 
 /**
@@ -176,7 +140,7 @@ class Sudoku {
  * Validation Area - would be in another file, but import doesn't work, somehow..
  * Rules:
  * - n must be unique in row and column
- * - n must be unique in its square (3x3 grid, aka gridToCheck)
+ * - n must be unique in its square (3x3 grid, aka smallerGrid)
  * 
  * ************************************************************************************************************************************
  */
@@ -185,104 +149,110 @@ class Sudoku {
     // check row and column
     // return validation state
     isValid(pos_x, pos_y, n) {
-        let isValid = true;    
-        // get the 3x3 grid to check
-        let gridToCheck = this.splitGrid(pos_x, pos_y);
+        let isValid = true; 
+        // initialize the 3x3 grid to check
+        this.splitGrid(pos_x, pos_y);
 
         // Check if row (pos_x) or collumn (pos_y) contains n already
-        for(let x = 0; x < this.grid.length; x++) {
-            if(this.grid[x][pos_y] == n || this.grid[pos_x][x] == n) {
+        // Don't check the current spot,
+        // cause then the check always fails -> important for isSolveable() method
+        for(let i = 0; i < this.grid.length; i++) {
+            // row                                        || column
+            if((this.grid[i][pos_y] == n) && (i != pos_x) || (this.grid[pos_x][i] == n) && (i != pos_y)) {
                 isValid = false;
-                return isValid; 
+                return isValid;
             }
         }
 
+        // Also skip the current spot,
+        // cause then the check will also fail for the isSolveable() method
         for(let x = 0; x < 3; x++) {
             for(let y = 0; y < 3; y++) {
-                if(gridToCheck[x][y] == n) {
+                if(this.smallerGrid[x][y] == n && !(x === pos_x % 3 && y === pos_y % 3)) {
                     isValid = false;
-                    return isValid; 
+                    return isValid;
                 }
             }
         }
-
         return isValid;
     }
 
     // You have a big grid of size 9x9
     // ...and wan't to make 9 smaller grids of size 3x3
     // ...but only the important one has to be touched
+    // Room for optimization.. 
     splitGrid(pos_x, pos_y) {
         // first section
         if(pos_x < 3 && pos_y < 3) {
             for(let x = 0; x < 3; x++) {
                 for(let y = 0; y < 3; y++) {
-                    this.grid1[x][y] = this.grid[x][y]; 
+                    this.smallerGrid[x][y] = this.grid[x][y]; 
                 }
             }
-            console.log(this.grid1);
-            return this.grid1;
         } else if(pos_x < 3 && pos_y > 2 && pos_y < 6 ) {
             for(let x = 0; x < 3; x++) {
                 for(let y = 3; y < 6; y++) {
-                    this.grid2[x][y - 3] = this.grid[x][y]; 
+                    this.smallerGrid[x][y - 3] = this.grid[x][y]; 
                 }
             }
-            return this.grid2;
         } else if(pos_x < 3 && pos_y > 5) {
             for(let x = 0; x < 3; x++) {
                 for(let y = 6; y < 9; y++) {
-                    this.grid3[x][y - 6] = this.grid[x][y]; 
+                    this.smallerGrid[x][y - 6] = this.grid[x][y]; 
                 }
             }
-            return this.grid3;
         // second section
         } else if(pos_x > 2 && pos_x < 6 && pos_y < 3) {
             for(let x = 3; x < 6; x++) {
                 for(let y = 0; y < 3; y++) {
-                    this.grid4[x - 3][y] = this.grid[x][y];  
+                    this.smallerGrid[x - 3][y] = this.grid[x][y];  
                 }
             }
-            return this.grid4;
         } else if(pos_x > 2 && pos_x < 6 && pos_y > 2 && pos_y < 6) {
             for(let x = 3; x < 6; x++) {
                 for(let y = 3; y < 6; y++) {
-                    this.grid5[x - 3][y - 3] = this.grid[x][y];  
+                    this.smallerGrid[x - 3][y - 3] = this.grid[x][y];  
                 }
             }
-            return this.grid5;
         } else if(pos_x > 2 && pos_x < 6 && pos_y > 5) {
             for(let x = 3; x < 6; x++) {
                 for(let y = 6; y < 9; y++) {
-                    this.grid6[x - 3][y - 6] = this.grid[x][y];  
+                    this.smallerGrid[x - 3][y - 6] = this.grid[x][y];  
                 }
             }
-            return this.grid6;
         // third section
         } else if(pos_x > 5 && pos_y < 3) {
             for(let x = 6; x < 9; x++) {
                 for(let y = 0; y < 3; y++) {
-                    this.grid7[x - 6][y] = this.grid[x][y];  
+                    this.smallerGrid[x - 6][y] = this.grid[x][y];  
                 }
             }
-            return this.grid7;
         } else if(pos_x > 5 && pos_y > 2 && pos_y < 6) {
             for(let x = 6; x < 9; x++) {
                 for(let y = 3; y < 6; y++) {
-                    this.grid8[x - 6][y - 3] = this.grid[x][y];  
+                    this.smallerGrid[x - 6][y - 3] = this.grid[x][y];  
                 }
             }
-            return this.grid8;
         } else if(pos_x > 5 && pos_y > 5) {
             for(let x = 6; x < 9; x++) {
                 for(let y = 6; y < 9; y++) {
-                    this.grid9[x - 6][y - 6] = this.grid[x][y];  
+                    this.smallerGrid[x - 6][y - 6] = this.grid[x][y];  
                 }
             }
-            return this.grid9;
         }
+    }
 
-        // Satisfying the Syntax
-        return null;
+    // Check beforehand, if the originalGrid is solveable
+    isSolveable() {
+        for (let x = 0; x < 9; x++) {
+            for (let y = 0; y < 9; y++) {
+                if (this.originalGrid[x][y] !== 0) {
+                    if (!this.isValid(x, y, this.originalGrid[x][y])) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
